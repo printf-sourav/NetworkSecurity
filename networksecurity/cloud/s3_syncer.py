87b7a0d0755f2f49d7e9da62a263aab0f5ed9996
printf-sourav <lcs.souravkrsahu@gmail.com>
2026-07-28 14:31:22 +0530

    cicd created

--- networksecurity/cloud/s3_syncer.py
+++ networksecurity/cloud/s3_syncer.py
@@ -1,12 +1,12 @@
-import subprocess
+
+import os
+
 
 class S3Sync:
-    def sync_folder_to_s3(self, folder, aws_bucket_url):
-        try:
-            command = f'aws s3 sync "{folder}" "{aws_bucket_url}"'
-            result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
-            print(result.stdout)
-        except subprocess.CalledProcessError as e:
-            print("AWS sync failed:")
-            print(e.stderr)
-            raise
+    def sync_folder_to_s3(self,folder,aws_bucket_url):
+        command = f"aws s3 sync {folder} {aws_bucket_url} "
+        os.system(command)
+
+    def sync_folder_from_s3(self,folder,aws_bucket_url):
+        command = f"aws s3 sync  {aws_bucket_url} {folder} "
+        os.system(command)
